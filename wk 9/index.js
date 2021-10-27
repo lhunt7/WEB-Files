00e0d0bcf15c00c5da6368581baca7738f2bb22c
lhunt7 <90488433+lhunt7@users.noreply.github.com>
2021-10-26 20:11:03 -0600

    Update index.js

--- wk 9/index.js
+++ wk 9/index.js
@@ -2,6 +2,6 @@ days = ["Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 
 for(i=0; i<document.getElementByClassName("days").length; i++){
   console.log(days[i]);
-  document.getElementByClassName("day")[i].id= meals[i];
+  document.getElementByClassName("day")[i].id= days[i];
 
 }
