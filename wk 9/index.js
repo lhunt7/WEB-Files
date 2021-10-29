2f08231a92de9ab9bfc1f6165592a83df4c94026
lhunt7 <90488433+lhunt7@users.noreply.github.com>
2021-10-28 18:03:56 -0600

    9

--- wk 9/index.js
+++ wk 9/index.js
@@ -1,9 +1,25 @@
 days = ["Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 
 for(i=0; i<document.getElementsByClassName("day").length; i++){
-  console.log(days[i]);
+  document.getElementsByClassName("day")[i].addEventListener('click',setActive, false);
   document.getElementsByClassName("day")[i].id = days[i];
+
+  document.getElementById("MonButton").addEventListener('click',setActive, false);
+  document.getElementById("TueButton").addEventListener('click',setActive, false);
+  document.getElementById("WedButton").addEventListener('click',setActive, false);
+  document.getElementById("ThuButton").addEventListener('click',setActive, false);
+  document.getElementById("FriButton").addEventListener('click',setActive, false);
+  document.getElementById("SatButton").addEventListener('click',setActive, false);
+  document.getElementById("SunButton").addEventListener('click',setActive, false);
+
 }
+
+function setActive(e){
+  e.target.classList.toggle("activeDay");
+  console.log(e.target.innerHTML);
+}
+
+
 
 const photoArray = ["calm-image-2.jpg","calm-image-3.jpg","calm-image-4.jpg","calm-image-5.jpeg","calm-image-6.jpg","calm-image-7.jpg","calm-image-1.jpg"];
 let index = 0;
